9a25fed4d845283c22a6eb70ad442b146de3c7db
Robertlanuza <rolanuza@my.cspc.edu.ph>
2025-03-25 15:09:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app AC")
+st.title("🎈Welcome to My new app AC")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
